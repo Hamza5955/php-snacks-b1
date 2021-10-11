273f9dc7280c2a4494703f0577d8efda40e8a589
Hamza <Hamza5955>
2021-10-11 11:45:00 +0200

    es3

---
+++ es3/index.php
@@ -0,0 +1,55 @@
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+foreach($posts as $key => $texts){
+    echo "<h1>Data: $key</h1>";
+    foreach($texts as $detas => $post){
+        foreach($post as $principale => $info){
+            if($principale === 'title'){
+                echo "<h1>$principale : $info</h1>";
+            }else{
+               echo "<h1>$principale : $info</h1>";
+            }
+        }
+    }
+}
+?>
